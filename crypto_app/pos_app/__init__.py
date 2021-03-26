11fb85c041daf81f18bff40d3b3ec6b12a1d24f2
karyam <zcabmka@ucl.ac.uk>
2021-03-26 10:30:33 +0000

    Add missing files

--- crypto_app/pos_app/__init__.py
+++ crypto_app/pos_app/__init__.py
@@ -1,1 +1,29 @@
 # init.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+
+    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///private_wallet.sqlite'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    app.config['msb1'] = 'cp:5000'
+    app.config['msb2'] = 'ap:5000'
+    app.config['msb3'] = 'service:5000'
+    
+    db.init_app(app)
+
+    #blueprint for auth routes in our app
+    # from private_wallet.auth import auth as auth_blueprint
+    # app.register_blueprint(auth_blueprint)
+
+    # # blueprint for non-auth parts of app
+    # from private_wallet.main import main as main_blueprint
+    # app.register_blueprint(main_blueprint)
+
+    db.create_all(app=app)
+    return app
