0dc3d317751553822bcff0003bde7feeaf3535a5
karyam <zcabmka@ucl.ac.uk>
2021-03-27 11:27:01 +0000

    init update

--- crypto_app/pos_app/__init__.py
+++ crypto_app/pos_app/__init__.py
@@ -6,14 +6,17 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
-
+    # database config
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///private_wallet.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-    app.config['msb1'] = 'cp:5000'
-    app.config['msb2'] = 'ap:5000'
-    app.config['msb3'] = 'service:5000'
+    # msb host config
+    app.config['msb1'] = 'msb1:5000'
+    app.config['msb2'] = 'msb2:5000'
+    app.config['msb3'] = 'msb3:5000'
+    app.config['msb4'] = 'msb4:5000'
+    app.config['msb5'] = 'msb5:5000'
     
     db.init_app(app)
 
