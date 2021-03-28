b3dddb430eb86a2193e8ae93711f961eb38af67b
samEmi <zcabose@ucl.ac.uk>
2021-03-28 18:08:00 +0100

    can add asset to user's msb account

--- experiment/main.py
+++ experiment/main.py
@@ -10,19 +10,12 @@ def addUsers(n):
         response = addUser(i)
         if response and response["success"]:
             token = response["token"]
-            print(token)
+            r = addAsset(token, 100)
+            print(r)
         else:
             print(response)
 
 
-def post(payload, url):
-    response = None
-    try:
-        response = requests.post(url, json=payload, timeout=180).json()
-    except:
-        print(f"Post Request Error ", e)
-        return response
-    return response
 
 def addUser(name):
     payload = {
@@ -30,15 +23,12 @@ def addUser(name):
         "orgName": "Org1"
     }
     return post(payload, url=f'{rootUrl}/users')
-    
-
-addUsers(4)
 
 
-def addAsset(fcn, address):
+def addAsset(address, amount):
     payload = {
         "fcn": "Mint",
-        "args": ["600"],
+        "args": [str(amount)],
         "peers": [
             "peer0.msb1.example.com",
             "peer0.msb2.example.com"
@@ -46,13 +36,41 @@ def addAsset(fcn, address):
         "chaincodeName": "token-erc-20",
         "channelName": "mychannel"
     }
-
-    headers = {"Authorization" : "Bearer " + str(address)}
-
-    r = requests.post(f'{rootUrl}/channels/mychannel/chaincodes/token-erc-20', json=payload, headers=headers, timeout=180)
-    print(r.text)
+    return invokeChaincodeFunc(address, payload)
 
 
+def transfer(sender, recipient, amount):
+    payload = {
+        "fcn": "Transfer",
+        "peers": [
+            "peer0.msb1.example.com",
+            "peer0.msb2.example.com"
+        ],
+        "chaincodeName": "token-erc-20",
+        "channelName": "mychannel",
+        "args": [
+            str(recipient),
+            str(amount)
+        ]
+    }
+    return invokeChaincodeFunc(sender, payload)
+
+def invokeChaincodeFunc(authorized_address, payload):
+    url = f'{rootUrl}/channels/mychannel/chaincodes/token-erc-20'
+    headers = {"Authorization" : "Bearer " + str(authorized_address)}
+    return post(payload, url, headers=headers)
+
+def post(payload, url, headers=None):
+    response = None
+    try:
+        response = requests.post(url, json=payload, headers=headers, timeout=180).json()
+    except Exception as e:
+        print(f"Post Request Error ", e)
+        return response
+    return response
+
+
+addUsers(4)
 
 # addAsset("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTY5MTUwNzQsInVzZXJuYW1lIjoiZmZmIiwib3JnTmFtZSI6Ik9yZzEiLCJpYXQiOjE2MTY4NzkwNzR9.hW8ZL3oMt7l1kblrbREL2_M5Xx87SP3dybGUGWhUnWg")
 
@@ -78,14 +96,3 @@ def addAsset(fcn, address):
 # r = requests.post('http://localhost:4000/channels/mychannel/chaincodes/token-erc-20', json=payload, headers=headers, timeout=180)
 
 # print(r.text)
-
-
-
-def post(payload, url):
-    response = None
-    try:
-        response = requests.post(url, json=payload, timeout=180).json()
-    except:
-        print(f"Post Request Error ", e)
-        return response
-    return response
