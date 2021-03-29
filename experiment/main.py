d7fb50d1d6b9f692ed9813c81a9e43b5611cd034
samEmi <zcabose@ucl.ac.uk>
2021-03-29 10:32:31 +0100

    latency against size

--- experiment/main.py
+++ experiment/main.py
@@ -1,93 +1,51 @@
-import requests
-import json
-from user import User, post
+from user import User, addUser
+import matplotlib.pyplot as plt
 
-rootUrl = "http://localhost:4000"
+monte_carlo_runs = 10
+nTransactions = 5
+networkSizes = [2, 10, 50, 100, 500, 1000]
 
 
-# def addUsers(n):
-#     for i in range(n):
-#         response = addUser(i)
-#         if response and response["success"]:
-#             token = response["token"]
-#             addAsset(token, 100)
-#             perform_transactions(token, 10, n)
-#         else:
-#             print(response)
-#         print("=================================================\n")
+def simulate():
+    avg_latencies = []
+    for size in networkSizes:
+        avg_latencies.append(avg_monte_carlo_latency(size))
+    return avg_latencies
 
-def simulate_network(size):
+def avg_monte_carlo_latency(size):
+    total = 0
+    for i in range(monte_carlo_runs):
+        print("Network Size ", size, "run ", i + 1)
+        total += get_avg_latency(size)
+    return total/monte_carlo_runs
+
+def get_avg_latency(size):
+    latencyList = []
+    userList = []
     for i in range(size):
         response = addUser(i)
         if response and response["success"]:
             token = response["token"]
-            user = User(token, 100, size, 9)
+            user = User(token, init_value=100, size=size, amount=1, nTransactions=nTransactions, latencyList=latencyList)
             user.start()
+            # time.sleep(3)
+            userList.append(user)
         else:
             print(response)
-        print("=================================================\n")
+    for user in userList: user.join()
+    avgLatency = sum(latencyList)/len(latencyList)
+    # print("List ", latencyList)
+    # print("avg ", avgLatency)
+    return avgLatency
 
-def addUser(name):
-    payload = {
-        "username": str(name),
-        "orgName": "Org1"
-    }
-    return post(payload, url=f'{rootUrl}/users')
+def plot_latency_size(avg_latencies):
+    plt.plot(networkSizes, avg_latencies)
+    plt.xlabel("Network Size")
+    plt.ylabel("Average Latency")
+    plt.title("Measuring Average Latency against Network Size")
+    plt.show()
 
-
-# def addAsset(address, amount):
-#     payload = {
-#         "fcn": "Mint",
-#         "args": [str(amount)],
-#         "peers": [
-#             "peer0.msb1.example.com",
-#             "peer0.msb2.example.com"
-#         ],
-#         "chaincodeName": "token-erc-20",
-#         "channelName": "mychannel"
-#     }
-#     return invokeChaincodeFunc(address, payload)
-
-
-# def perform_transactions(sender, amount, n):
-#     for i in range(n):
-#         r = transfer(sender, i, amount)
-#         print(r)
-    
-
-# def transfer(sender, recipient, amount):
-#     payload = {
-#         "fcn": "Transfer",
-#         "peers": [
-#             "peer0.msb1.example.com",
-#             "peer0.msb2.example.com"
-#         ],
-#         "chaincodeName": "token-erc-20",
-#         "channelName": "mychannel",
-#         "args": [
-#             str(recipient),
-#             str(amount)
-#         ]
-#     }
-#     return invokeChaincodeFunc(sender, payload)
-
-
-# def invokeChaincodeFunc(authorized_address, payload):
-#     url = f'{rootUrl}/channels/mychannel/chaincodes/token-erc-20'
-#     headers = {"Authorization" : "Bearer " + str(authorized_address)}
-#     return post(payload, url, headers=headers)
-
-
-# def post(payload, url, headers=None):
-#     response = None
-#     try:
-#         response = requests.post(url, json=payload, headers=headers, timeout=180).json()
-#     except Exception as e:
-#         print(f"Post Request Error ", e)
-#         return response
-#     return response
-
-
-simulate_network(4)
+avg_latencies = simulate()
+plot_latency_size(avg_latencies)
 
 
--- experiment/user.py
+++ experiment/user.py
@@ -3,24 +3,26 @@ import random
 from threading import Thread
 
 random.seed(10)
+rootUrl = "http://localhost:4000"
 
 class User(Thread):
-    def __init__(self, address, value, n, amount):
+    def __init__(self, address, init_value, size, amount, nTransactions, latencyList):
           super().__init__()
           self.address = address
-          self.value = value
-          self.n = n
+          self.value = init_value
+          self.size = size
           self.amount = amount
-          self.rootUrl = "http://localhost:4000"
+          self.nTransactions = nTransactions
+          self.latencyList = latencyList
     
     def run(self):
-        self.addAsset(self.value)
-        self.perform_transactions(self.amount, self.n)
+        self.addAsset()
+        self.perform_transactions()
     
-    def addAsset(self, value):
+    def addAsset(self):
         payload = {
             "fcn": "Mint",
-            "args": [str(value)],
+            "args": [str(self.value)],
             "peers": [
                 "peer0.msb1.example.com",
                 "peer0.msb2.example.com"
@@ -31,14 +33,19 @@ class User(Thread):
         return self.helper(payload)
     
 
-    def perform_transactions(self, amount, n):
-        for i in range(n):
-            randInt = random.randint(0, n)
-            r = self.transfer(randInt, amount)
+    def perform_transactions(self):
+        success = 0
+        total = 0
+        for i in range(self.nTransactions):
+            randInt = random.randint(0, self.size)
+            r = self.transfer(randInt, self.amount)
             if r["result"]:
-                print("Response ", r, "Recipient ", randInt, "Thread name ", self.name)
-            else:
-                print(":(((", r)
+                # print("Response ", r, "Recipient ", randInt, "Thread name ", self.name)
+                success += 1
+                total += r["latency"]
+            # else:
+            #     print(":(((", r, "Thread name ", self.name)
+        self.latencyList.append(total/success)
     
 
     def transfer(self, recipient, amount):
@@ -58,25 +65,25 @@ class User(Thread):
         return self.helper(payload)
 
     def helper(self, payload):
-        url = f'{self.rootUrl}/channels/mychannel/chaincodes/token-erc-20'
+        url = f'{rootUrl}/channels/mychannel/chaincodes/token-erc-20'
         headers = {"Authorization" : "Bearer " + str(self.address)}
         return post(payload, url, headers=headers)
 
+
+def addUser(name):
+    payload = {
+        "username": str(name),
+        "orgName": "Org1"
+    }
+    return post(payload, url=f'{rootUrl}/users')
 
 
 def post(payload, url, headers=None):
     response = None
     try:
         response = requests.post(url, json=payload, headers=headers, timeout=180).json()
+        # if not response["result"]: print(payload)
     except Exception as e:
         print(f"Post Request Error ", e)
         return response
     return response
-    # def post(self, payload, url, headers=None):
-    #     response = None
-    #     try:
-    #         response = requests.post(url, json=payload, headers=headers, timeout=180).json()
-    #     except Exception as e:
-    #         print(f"Post Request Error ", e)
-    #         return response
-    #     return response
