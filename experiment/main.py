48e037433d447e098c23b84f4a4df29c56f8e065
samEmi <zcabose@ucl.ac.uk>
2021-03-29 20:22:20 +0100

    latency against nTransactions

--- experiment/main.py
+++ experiment/main.py
@@ -2,31 +2,46 @@ from user import User, addUser
 import matplotlib.pyplot as plt
 
 monte_carlo_runs = 10
+# Constants for the latency against network size graph
 nTransactions = 5
-networkSizes = [2, 10, 50, 100, 500, 1000]
+networkSizes = [2, 10, 25, 50, 100, 200]
+# Constants for the latency against numberOfTransactions per user graph
+network_size = 5
+transactions = [5, 10, 25, 50, 75, 100, 150, 200] # each value represents the number of transactions per user for an experiment
 
-
-def simulate():
+# Gets values for the latency against network size graph
+def latency_against_size():
     avg_latencies = []
     for size in networkSizes:
-        avg_latencies.append(avg_monte_carlo_latency(size))
+        avg_latency = avg_monte_carlo_latency(size, nTransactions)
+        print("Avg Latency : ", avg_latency)
+        avg_latencies.append(avg_latency)
     return avg_latencies
 
-def avg_monte_carlo_latency(size):
+# Gets values for the latency against numberOfTransactions per user graph
+def latency_against_transactions():
+    avg_latencies = []
+    for transaction in transactions:
+        avg_latency = avg_monte_carlo_latency(network_size, transaction)
+        print("Avg Latency : ", avg_latency)
+        avg_latencies.append(avg_latency)
+    return avg_latencies
+
+def avg_monte_carlo_latency(size, transactions):
     total = 0
     for i in range(monte_carlo_runs):
-        print("Network Size ", size, "run ", i + 1)
-        total += get_avg_latency(size)
+        print("Network Size ", size, "nTransactionsPerUser", transactions, "run ", i + 1)
+        total += get_avg_latency(size, transactions)
     return total/monte_carlo_runs
 
-def get_avg_latency(size):
+def get_avg_latency(size, transactions):
     latencyList = []
     userList = []
     for i in range(size):
         response = addUser(i)
         if response and response["success"]:
             token = response["token"]
-            user = User(token, init_value=100, size=size, amount=1, nTransactions=nTransactions, latencyList=latencyList)
+            user = User(token, init_value=1000, size=size, amount=1, nTransactions=transactions, latencyList=latencyList)
             user.start()
             # time.sleep(3)
             userList.append(user)
@@ -38,14 +53,17 @@ def get_avg_latency(size):
     # print("avg ", avgLatency)
     return avgLatency
 
-def plot_latency_size(avg_latencies):
-    plt.plot(networkSizes, avg_latencies)
-    plt.xlabel("Network Size")
-    plt.ylabel("Average Latency")
-    plt.title("Measuring Average Latency against Network Size")
+def plot_latency_size(avg_latencies, x, xLabel='Number of users'):
+    plt.plot(x, avg_latencies)
+    plt.xlabel(xLabel)
+    plt.ylabel("Average Latency / ms")
+    plt.title(f"Average Latency against {xLabel}")
     plt.show()
 
-avg_latencies = simulate()
-plot_latency_size(avg_latencies)
+avg_latencies = latency_against_size()
+plot_latency_size(avg_latencies, networkSizes, xLabel='Number of users')
+
+avg_latencies = latency_against_transactions()
+plot_latency_size(avg_latencies, transactions, xLabel='Number of transactions per user')
 
 
