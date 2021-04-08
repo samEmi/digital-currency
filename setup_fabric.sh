a5228c5afe054527c60f4e1b180d9575c09357d7
karyam <zcabmka@ucl.ac.uk>
2021-04-08 11:21:04 +0000

    setup

--- setup_fabric.sh
+++ setup_fabric.sh
@@ -1,8 +1,4 @@
 # run outside project dir
-sudo apt-get install golang
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
-
 sudo apt-get install nodejs
 
 sudo apt-get install npm
@@ -10,3 +6,19 @@ sudo apt-get install npm
 sudo curl -sSL https://bit.ly/2ysbOFE | bash -s
 
 sudo cp ./fabric-samples/bin/* /usr/local/bin
+
+# cd into project dir
+cd digital-currency
+
+chmod +x ./install_dependencies.sh
+./install_dependencies.sh
+
+cd network-config/artifacts
+docker-compose up -d
+
+cd ..
+chmod +x ./createChannel.sh
+./createChannel.sh
+
+chmod +x ./deployChaincode.sh
+./deployChaincode.sh
