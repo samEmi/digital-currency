4a7b98aa3ea185877c30f319ee2c0677382269d5
Samuel <zcabose@ucl.ac.uk>
2021-04-09 14:47:14 +0100

    changes in config

--- network-config/artifacts/channel/create-artifacts.sh
+++ network-config/artifacts/channel/create-artifacts.sh
@@ -1,9 +1,9 @@
 
-# chmod -R 0755 ./crypto-config
-# # Delete existing artifacts
-# rm -rf ./crypto-config
-# rm genesis.block mychannel.tx
-# rm -rf ../../channel-artifacts/*
+chmod -R 0755 ./crypto-config
+# Delete existing artifacts
+rm -rf ./crypto-config
+rm genesis.block mychannel.tx
+rm -rf ../../channel-artifacts/*
 
 #Generate Crypto artifactes for organizations
 cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
